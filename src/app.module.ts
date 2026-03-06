a49ccf02ef476d0320beb33ee4e2d76358fcc3b1
Abdo Nasser <abdonasser865@gmail.com>
2026-03-06 22:38:51 +0200

    feat: Implement user authentication, CRUD operations, and Prisma ORM setup for the API.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { PrismaService } from './modules/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: config.get('JWT_EXPIRATION_TIME'),
+          },
+        };
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
